e68b391ba2f2de7bbc3972df981e7c644c1dbeca
IgnReLoaD <ortiz.ignasi@gmail.com>
2022-10-27 12:20:17 +0200

    estructura de carpetes de Sprint-3 tema-Patterns

---
+++ 3_Behavior_Patterns/cupoun.php
@@ -0,0 +1,18 @@
+function cupounGenerator($car) {
+
+    $discount = 0;
+    $isHighSeason = false;
+    $bigStock = true;
+
+    if($car == "bmw"){
+        if(!$isHighSeason) {$discount += 5;}
+    if($bigStock) {$discount += 7;}
+    } else if($car == "mercedes") {
+    if(!$isHighSeason) {$discount += 4;}
+    if($bigStock) {$discount += 10;}
+
+    }
+    return $cupoun = "Get {$discount}% off the price of your new car.";
+}
+
+echo cupounGenerator("bmw");
