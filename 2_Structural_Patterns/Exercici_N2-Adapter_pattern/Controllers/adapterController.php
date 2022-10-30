bb6be194499c517dbbc90d41fdf250f5189fcde7
IgnReLoaD <ortiz.ignasi@gmail.com>
2022-10-30 15:02:12 +0100

    desesperante, no hay forma que ejecute globbe()

--- 2_Structural_Patterns/Exercici_N2-Adapter_pattern/Controllers/adapterController.php
+++ 2_Structural_Patterns/Exercici_N2-Adapter_pattern/Controllers/adapterController.php
@@ -1,22 +1,28 @@
 <?php
 
-require "../Models/adapterModel.php";
+    require "../Models/adapterModel.php";
 
-function duck_interaction($duck) {
-    $duck->quack();
-    $duck->fly();
-}
+    function duck_interaction($duck) {
+        $duck->quack();
+        $duck->fly();
+    }
 
-$duck = new Duck;
-$turkey = new Turkey;
-// cal implementar la classe TurkeyAdapter en adapterModel
-// $turkey_adapter = new TurkeyAdapter($turkey);
-echo "The Turkey says...\n";
-$turkey->gobble();
-$turkey->fly();
-echo "The Duck says...\n";
-duck_interaction($duck);
-echo "The TurkeyAdapter says...\n";
-duck_interaction($turkey_adapter);
+    $duck = new Duck;
+    $turkey = new Turkey;
+    // cal implementar la classe TurkeyAdapter en adapterModel
+    $turkey_adapter = new TurkeyAdapter($turkey);
+
+    // sortida de dades Turkey (pavo)
+    echo "<br><h3>" . "The Turkey says...\n" . "</h3><br>";
+    $turkey->gobble();
+    $turkey->fly();
+
+    // sortida de dades Duck (pato)
+    echo "<br><h3>" . "The Duck says...\n" . "</h3><br>";
+    duck_interaction($duck);
+
+    // sortida de dades TurkeyAsDuck (pavoComoPato)
+    echo "<br><h3>" . "The TurkeyAdapter says...\n" . "</h3><br>";
+    duck_interaction($turkey_adapter);
 
 ?>
--- 2_Structural_Patterns/Exercici_N2-Adapter_pattern/Models/adapterModel.php
+++ 2_Structural_Patterns/Exercici_N2-Adapter_pattern/Models/adapterModel.php
@@ -3,27 +3,51 @@
     class Duck {
 
         public function quack() {
-            echo "Quack \n";
+            echo "Quack \n" . "<br>";
         }
 
         public function fly() {
-            echo "I'm flying \n";
+            echo "I'm flying \n" . "<br>";
         }
-        }
+    }
 
-        class Turkey {
+    class Turkey {
 
         public function gobble() {
-            echo "Gobble gobble \n";
+            echo "Gobble gobble \n" . "<br>";
         }
 
         public function fly() {
-        echo "I'm flying a short distance \n";
+        echo "I'm flying a short distance \n" . "<br>";
         }
     }
 
     // aquí caldria desenvolupar una classe, que actui com Adapter
     // capturant les trucades a classe Turkey i modificant com Duck
     // ......
+    class TurkeyAdapter extends Turkey{
     
+        // 1. Zona atributs
+        private $turkeyAsDuck;
+    
+        // 2. Zona constructor
+        public function __construct(Turkey $turkeyLearning){
+            $this->turkeyAsDuck = $turkeyLearning;
+        }
+
+        // 2. Zona mètodes, en tindrem 2:
+
+        // capturem la trucada a "quack" (mateix nom) perquè faci un globe
+        public function quack(){
+            $this->turkeyAsDuck->globbe();
+        }
+
+        // capturem la trucada a "fly" (mateix nom) per adaptar llargada del vol
+        public function fly(){
+            for ($step=1;$step<=5;$step++){
+                $this->turkeyAsDuck->fly();
+            }
+        }
+    }
+
 ?>
